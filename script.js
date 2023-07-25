59ee939984d53cc41742bcdc0572343ba2c11b35
Duvan2028 <126742493+Duvan2028@users.noreply.github.com>
2023-07-25 02:08:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   const tableBody = document.getElementById('table-body');
   const sumTypeSelect = document.getElementById('sum-type');
+  const sumRangeSelect = document.getElementById('sum-range');
+  const countryOrContinentInput = document.getElementById('country-or-continent');
+  const infoLink = document.getElementById('info-link');
 
   // Ruta de tu archivo CSV con datos de sum of ranks (puedes tener diferentes rutas para cada tipo y nivel)
   const csvFileURL = 'URL_DEL_ARCHIVO_CSV';
@@ -34,9 +37,32 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // Cargar datos al cargar la página y cuando se cambia el tipo de sum of ranks
+  // Función para mostrar el enlace de la lista oficial de continentes o países
+  function showInfoLink() {
+    const sumRange = sumRangeSelect.value;
+    const name = countryOrContinentInput.value;
+    let link = '';
+
+    if (sumRange === 'continental') {
+      link = `<a href="https://www.worldcubeassociation.org/regions/" target="_blank">Lista de Continentes</a>`;
+    } else if (sumRange === 'pais') {
+      link = `<a href="https://www.worldcubeassociation.org/countries/" target="_blank">Lista de Países</a>`;
+    }
+
+    infoLink.innerHTML = link;
+  }
+  // Cargar datos al cargar la página y cuando se cambia el tipo de sum of ranks o rango
   loadCSVData(csvFileURL);
   sumTypeSelect.addEventListener('change', function() {
     loadCSVData(csvFileURL);
   });
+  sumRangeSelect.addEventListener('change', function() {
+    loadCSVData(csvFileURL);
+    showInfoLink();
+  });
+  countryOrContinentInput.addEventListener('input', function() {
+    loadCSVData(csvFileURL);
+    showInfoLink();
+  });
 });
+ 
