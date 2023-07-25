de4b3cb1c3cfc69cf51578b0b66db1094c0efbbf
Duvan2028 <126742493+Duvan2028@users.noreply.github.com>
2023-07-25 02:30:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
   const sumRangeSelect = document.getElementById('sum-range');
   const countryOrContinentInput = document.getElementById('country-or-continent');
   const infoLink = document.getElementById('info-link');
+  const csvFileSelect = document.getElementById('csv-file');
 
-  // Ruta de tu archivo CSV con datos de sum of ranks (puedes tener diferentes rutas para cada tipo y nivel)
-  const csvFileURL = 'URL_DEL_ARCHIVO_CSV';
-
-  // Función para cargar los datos CSV
-  function loadCSVData(url) {
-    Papa.parse(url, {
+  // Función para cargar los datos CSV seleccionados por el usuario
+  function loadSelectedCSV() {
+    const selectedCSV = csvFileSelect.value;
+    Papa.parse(selectedCSV, {
       download: true,
       header: true,
       complete: function(results) {
@@ -20,63 +19,22 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // Función para mostrar el enlace de la lista oficial de continentes o países
-  function showInfoLink() {
-    const sumRange = sumRangeSelect.value;
-    const name = countryOrContinentInput.value;
-    let link = '';
+  // Resto del código JavaScript (displayTableData, showInfoLink, etc.)
 
-    if (sumRange === 'continental') {
-      link = `<a href="https://www.worldcubeassociation.org/wfc/country-bands" target="_blank">Lista de Continentes</a>`;
-    } else if (sumRange === 'pais') {
-      link = `<a href="https://www.worldcubeassociation.org/regulations/countries/" target="_blank">Lista de Países</a>`;
-    }
-
-    infoLink.innerHTML = link;
-  }
-
-  // Función para mostrar los datos en la tabla con encabezados y columnas dinámicas
-  function displayTableData(data) {
-    tableBody.innerHTML = '';
-    const sumType = sumTypeSelect.value;
-    const filteredData = data.filter(row => row['Tipo'] === sumType).slice(0, 100);
-
-    // Obtener los encabezados de la tabla a partir de las claves del primer objeto del arreglo
-    const headers = Object.keys(filteredData[0]);
-
-    // Crear los encabezados de la tabla
-    const headerRow = document.createElement('tr');
-    headers.forEach(header => {
-      const th = document.createElement('th');
-      th.textContent = header;
-      headerRow.appendChild(th);
-    });
-    tableBody.appendChild(headerRow);
-
-    // Crear las filas de la tabla con los datos
-    filteredData.forEach(row => {
-      const newRow = document.createElement('tr');
-      headers.forEach(header => {
-        const td = document.createElement('td');
-        td.textContent = row[header];
-        newRow.appendChild(td);
-      });
-      tableBody.appendChild(newRow);
-    });
-  }
-
-  // Cargar datos al cargar la página y cuando se cambia el tipo de sum of ranks o rango
-  loadCSVData(csvFileURL);
+  // Cargar datos al cargar la página y cuando se cambia el tipo de sum of ranks, rango o archivo CSV
+  loadSelectedCSV();
   sumTypeSelect.addEventListener('change', function() {
-    loadCSVData(csvFileURL);
+    loadSelectedCSV();
   });
   sumRangeSelect.addEventListener('change', function() {
-    loadCSVData(csvFileURL);
+    loadSelectedCSV();
     showInfoLink();
   });
   countryOrContinentInput.addEventListener('input', function() {
-    loadCSVData(csvFileURL);
+    loadSelectedCSV();
     showInfoLink();
   });
+  csvFileSelect.addEventListener('change', function() {
+    loadSelectedCSV();
+  });
 });
-        
